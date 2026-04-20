e88841687ac4f88a81f18c889e9a91389d805fe2
EmilioAugust <kuluev.emil.8@gmail.com>
2026-04-20 16:23:52 +0500

    Added pages/cart_page.py

---
+++ pages/cart_page.py
@@ -0,0 +1,14 @@
+from pages.base_page import BasePage
+from utils.config import CART_URL
+from locators.cart_locator import CartLocator
+
+class CartPage(BasePage):
+    def open(self):
+        super().open(CART_URL)
+
+    def remove_from_cart(self):
+        self.open()
+        self.click(CartLocator.REMOVE_BTN)
+
+    def is_item_in_cart_visible(self):
+        return self.is_visible(CartLocator.PRODUCT)
